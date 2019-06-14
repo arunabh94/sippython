78c411e67caf6579fd1a3f1ee9c1150984063d8c
arunabh94 <51732640+arunabh94@users.noreply.github.com>
2019-06-14 10:14:53 +0530

    14th June

---
+++ assignment1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s 
+
+@author: %(arunabh)s
+"""
+
+
+list2=['a','b','c','d']
+list2
+type(list2)
+
+tuple1=(5,6,'a','b')
+tuple1
+type(tuple1)
+
+dict1={1:'Ravi',2:'Shyam',3:'Rita'}
+dict1
+type(dict1)
+
+set1=(['Argentina','England','Portugal','Spain','Brazil'])
+set1
+type(set1)
+
+str1='Python Programming'
+str1
+type(str1)
